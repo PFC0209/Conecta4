ec07bc2213022cdf36c3b61795b77e5c2d0b851d
Pau Febrer Castro <pfebrerc@gmail.com>
2025-02-06 12:36:35 +0100

    Implementació de Tablero

--- src/Conecta4/Tablero.java
+++ src/Conecta4/Tablero.java
@@ -1,4 +1,18 @@
 package Conecta4;
 
+import static Conecta4.Conecta4.*;
+
 public class Tablero {
+    public void imprimirTablero() {
+        System.out.println("\n 1 2 3 4 5 6 7 ");
+        for (int i = 0; i < files;i++){
+            System.out.println("|");
+            for (int j = 0; j < columnes;j++){
+                System.out.print(tablero[i][j] + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------");
+    }
+
 }
