972eebfbf0d543e405022fd3dcb32a545f3ea2a5
Pau Febrer Castro <pfebrerc@gmail.com>
2025-02-06 09:36:21 +0100

    Creació de variables Final

--- src/Conecta4/Conecta4.java
+++ src/Conecta4/Conecta4.java
@@ -1,5 +1,9 @@
 package Conecta4;
-
+import java.util.Scanner;
 public class Conecta4{
-
+    public static final int files = 6;
+    public static final int columnes = 7;
+    public static final char buit = ' ';
+    public static final char jugador1 = 'X';
+    public static final char jugador2 = 'O';
 }
