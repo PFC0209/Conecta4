76137e418c784a5006aedb02bd78258ebc335e43
Pau Febrer Castro <pfebrerc@gmail.com>
2025-02-06 10:32:25 +0100

    Variable Tablero

--- src/Conecta4/Conecta4.java
+++ src/Conecta4/Conecta4.java
@@ -6,4 +6,17 @@ public class Conecta4{
     public static final char buit = ' ';
     public static final char jugador1 = 'X';
     public static final char jugador2 = 'O';
+
+    //tablero
+    public static char [][] tablero;
+
+
+    public Conecta4(){
+        tablero = new char[files][columnes];
+        for(int i = 0; i < files; i++){
+            for(int j = 0; j < columnes; j++){
+                tablero[i][j] = buit;
+            }
+        }
+    }
 }
