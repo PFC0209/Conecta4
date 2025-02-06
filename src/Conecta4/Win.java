3f377c97a2d6a135e537705a4199bf41d03c751b
Pau Febrer Castro <pfebrerc@gmail.com>
2025-02-06 12:39:34 +0100

    Implementació de la classe Win

--- src/Conecta4/Win.java
+++ src/Conecta4/Win.java
@@ -1,4 +1,35 @@
 package Conecta4;
 
+import static Conecta4.Conecta4.*;
+
 public class Win {
+    public boolean HihaGuanyador(char jugador){
+        //Comporovació horitzontal
+        for(int i = 0; i < files; i++){
+            for(int j = 0; j <= columnes - 4; j++){
+                if(tablero[i][j] == jugador && tablero[i][j+1] == jugador && tablero[i][j+2] == jugador && tablero[i][j+3] == jugador){
+                    return true;
+                }
+            }
+        }
+        //Comprovació vertical
+
+        for(int i = 0; i <= files -4; i++){
+            for(int j = 0; j < columnes; j++){
+                if(tablero[i][j] == jugador && tablero[i+1][j] == jugador && tablero[i+2][j] == jugador && tablero[i+3][j] == jugador){
+                    return true;
+                }
+            }
+        }
+        //Comprovació diagonal
+
+        for(int i = 3; i < files; i++){
+            for(int j = 0; j <= columnes - 4; j++){
+                if (tablero[i][j] == jugador && tablero[i-1][j+1] == jugador && tablero[i-2][j+2] == jugador && tablero[i-3][j+3] == jugador){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
